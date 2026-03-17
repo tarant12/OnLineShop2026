351db7d016ac372fd6392d165cea7749765c14d5
Anton Taraskin <akdh060909@gmail.com>
2026-03-17 09:59:50 +0300

    добавили сохранение фотографии локально

--- OnLineShop2026/Data/ProductRepository.cs
+++ OnLineShop2026/Data/ProductRepository.cs
@@ -6,7 +6,7 @@ namespace OnLineShop2026.Data
     {
         private static List<Product> products = new List<Product>()
         {
-            new Product("Пирожок", "с котятами", 50, "https://cs14.pikabu.ru/post_img/2022/03/21/6/1647856655122391644.jpg"),
+            new Product("Пирожок", "с котятами", 50, "/images/mem.jpg"),
             new Product("Чебурек", "с вишней", 80, "https://optim.tildacdn.com/tild3635-3764-4331-b832-633037643133/-/resize/340x/-/format/webp/cheb.png.webp"),
             new Product("Тортик", "медовик", 90, "https://foodmood.ru/upload/iblock/f18/f18a6cb201f83cf11e2cc72f694eb3d6.jpg")
         };
